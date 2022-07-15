4fb95ca6c58aa96c4f09776d8965898f3a4cec97
chminipark <chminipark@gmail.com>
2022-07-15 19:22:30 +0900

    Feat: First init
    Remove StoryBoard
    Pod init
    Pod install Snapkit, Then
    Create .gitignore
    Create README.md

---
+++ GitHubSearch/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  GitHubSearch
+//
+//  Created by chmini on 2022/07/15.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else {
+            return
+        }
+        
+        window = UIWindow(windowScene: windowScene)
+        let mainViewController = ViewController()
+        window?.rootViewController = mainViewController
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ GitHubSearch/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  GitHubSearch
+//
+//  Created by chmini on 2022/07/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}

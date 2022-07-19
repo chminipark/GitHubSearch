a426c3a57da26eadbe6863277ac5c7bdb5f54193
chminipark <chminipark@gmail.com>
2022-07-19 15:08:37 +0900

    [UI] : Create TableView in SearchViewController
    Related to: #1

--- GitHubSearch/Views/SearchViewController.swift
+++ GitHubSearch/Views/SearchViewController.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class SearchViewController: UIViewController {
     
+    private var tableView: UITableView!
+    
+    let data: Array<String> = {
+        var data = [String]()
+        for i in stride(from: 1, through: 50, by: 1) {
+            data.append(String(i))
+        }
+        return data
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
@@ -16,17 +26,48 @@ class SearchViewController: UIViewController {
     
     private func setUI() {
         view.backgroundColor = .white
-        
         self.navigationItem.title = "GitHub Search"
         self.navigationController?.navigationBar.prefersLargeTitles = true
-        setSearchBar()
+        
+        configureSearchBar()
+        configureTableView()
     }
     
-    private func setSearchBar() {
+    private func configureSearchBar() {
         let searchController = UISearchController(searchResultsController: nil)
         self.navigationItem.searchController = searchController
-        searchController.hidesNavigationBarDuringPresentation = false
         self.navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.hidesNavigationBarDuringPresentation = false
+    }
+    
+    private func configureTableView() {
+        tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+}
+
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = data[indexPath.row]
+        cell.contentConfiguration = content
+        
+        return cell
     }
     
 }

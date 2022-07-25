ba9d9d2a0f6a52e16becd782cf442aee89203d05
chminipark <chminipark@gmail.com>
2022-07-25 14:55:46 +0900

    [Feat] : Repo Entity, GitHubSearchResponseDTO 생성
    Ref: #3
    Related to: #8

---
+++ GitHubSearch/Data/Repository/GitHubSearchRepository.swift
@@ -0,0 +1,8 @@
+//
+//  GitHubSearchRepository.swift
+//  GitHubSearch
+//
+//  Created by chmini on 2022/07/24.
+//
+
+import Foundation
---
+++ GitHubSearch/Domain/Entity/Repo.swift
@@ -0,0 +1,14 @@
+//
+//  Repo.swift
+//  GitHubSearch
+//
+//  Created by chmini on 2022/07/24.
+//
+
+import Foundation
+
+struct Repo {
+    let name: String
+    let fullName: String
+    let starCount: Int
+}
